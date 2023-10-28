c35972d0bcd19c11938796f25f3c4d66c82e492e
Yasir <DPSA00609@mypaceschool.com>
2023-10-28 20:51:13 +0400

    Implementing the edit and delete records methods

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QLayout, QWidget, QGridLayout,
                              QLabel, QLineEdit, QPushButton, QMainWindow,
                              QTableWidget, QTableWidgetItem, QDialog,
-                             QVBoxLayout, QComboBox, QToolBar)
+                             QVBoxLayout, QComboBox, QToolBar, QStatusBar,
+                             QMessageBox)
 from PyQt6.QtGui import QAction, QIcon
 import sqlite3
 import sys
@@ -59,6 +60,12 @@ class MainWindow(QMainWindow):
         add_student_toolbar_action.triggered.connect(
             search_student_action.trigger)
 
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
+        self.status_bar.hide()
+
+        self.table.cellClicked.connect(self.cell_clicked)
+
     def load_data(self):
         connection = sqlite3.connect("database.db")
         result = list(connection.execute("SELECT * FROM students"))
@@ -77,6 +84,30 @@ class MainWindow(QMainWindow):
     def search(self):
         dialog = SearchDialog(self.table)
         dialog.exec()
+
+    def edit(self):
+        dialog = EditRecord(self.table)
+        dialog.exec()
+
+    def remove(self):
+        dialog = RemoveRecord(self.table)
+        dialog.exec()
+
+    def cell_clicked(self):
+        self.status_bar.show()
+        edit_button = QPushButton("Edit Record")
+        edit_button.clicked.connect(self.edit)
+
+        delete_button = QPushButton("Delete Record")
+        delete_button.clicked.connect(self.remove)
+
+        children = self.findChildren(QPushButton)
+        if children:
+            for child in children:
+                self.status_bar.removeWidget(child)
+
+        self.status_bar.addWidget(edit_button)
+        self.status_bar.addWidget(delete_button)
 
 
 class InsertDialog(QDialog):
@@ -104,6 +135,9 @@ class InsertDialog(QDialog):
         register_button.clicked.connect(self.add_student)
         layout.addWidget(register_button)
 
+        self.output = QLabel("")
+        layout.addWidget(self.output)
+
         self.setLayout(layout)
 
     def add_student(self):
@@ -116,6 +150,7 @@ class InsertDialog(QDialog):
         cursor.execute(
             "INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
             (name, course, phone_number))
+        self.output.setText("Inserted student details successfully.")
         connection.commit()
         cursor.close()
         connection.close()
@@ -140,6 +175,9 @@ class SearchDialog(QDialog):
         search_button.clicked.connect(self.search)
         layout.addWidget(search_button)
 
+        self.output = QLabel("")
+        layout.addWidget(self.output)
+
         self.setLayout(layout)
 
     def search(self):
@@ -153,10 +191,100 @@ class SearchDialog(QDialog):
                                      Qt.MatchFlag.MatchFixedString)
 
         for item in items:
+            self.table.item(item.row(), 0).setSelected(True)
             self.table.item(item.row(), 1).setSelected(True)
-
+            self.table.item(item.row(), 2).setSelected(True)
+            self.table.item(item.row(), 3).setSelected(True)
+            self.output.setText("Please close this window.")
         cursor.close()
         connection.close()
+
+
+class EditRecord(QDialog):
+    def __init__(self, table: QTableWidget):
+        super().__init__()
+        self.table = table
+        self.setWindowTitle("Edit Student Details")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        index = self.table.currentRow()
+        student_name = self.table.item(index, 1).text()
+        phone_number = self.table.item(index, 3).text()
+        course_name = self.table.item(index, 2).text()
+        self.id = self.table.item(index, 0).text()
+        self.student_name = QLineEdit(student_name)
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        self.courses_dropbox = QComboBox()
+        self.courses_dropbox.addItems(courses)
+        self.courses_dropbox.setCurrentText(course_name)
+        layout.addWidget(self.courses_dropbox)
+
+        self.phone_number = QLineEdit(phone_number)
+        self.phone_number.setPlaceholderText("Phone no.")
+        layout.addWidget(self.phone_number)
+
+        register_button = QPushButton("Update")
+        register_button.clicked.connect(self.edit_record)
+        layout.addWidget(register_button)
+
+        self.output = QLabel("")
+        layout.addWidget(self.output)
+
+        self.setLayout(layout)
+
+    def edit_record(self):
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute(
+            "UPDATE students SET name = ?, course = ?, mobile = ? "
+            "WHERE id = ?", (self.student_name.text(),
+                             self.courses_dropbox.itemText(
+                                 self.courses_dropbox.currentIndex()),
+                             self.phone_number.text(), self.id))
+        self.output.setText("Updated details successfully.")
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
+
+
+class RemoveRecord(QDialog):
+    def __init__(self, table: QTableWidget):
+        super().__init__()
+        self.table = table
+        self.setWindowTitle("Delete Student Details")
+        layout = QGridLayout()
+
+        confirmation = QLabel(
+            "Are you sure you want to remove student details?")
+        layout.addWidget(confirmation, 0, 0, 1, 2)
+
+        yes_button = QPushButton("Yes")
+        yes_button.clicked.connect(self.delete_record)
+        layout.addWidget(yes_button, 1, 0)
+
+        no_button = QPushButton("No")
+        no_button.clicked.connect(self.reject)
+        layout.addWidget(no_button, 1, 1)
+
+        self.setLayout(layout)
+
+    def delete_record(self):
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        index = self.table.currentRow()
+        id = self.table.item(index, 0).text()
+        cursor.execute("DELETE FROM students WHERE id=?", (id,))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        main_window.load_data()
+        self.reject()
 
 
 app = QApplication(sys.argv)
